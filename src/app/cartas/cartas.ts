cada45025826ca8a20fefd950acc0a15dcd135d7
samuelcastr <samuelcr11@outlook.com>
2025-11-05 12:29:46 -0500

    Second commit

--- src/app/cartas/cartas.ts
+++ src/app/cartas/cartas.ts
@@ -8,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class Cartas {
 
-  baraja =["😒","😍","😍","🎙️","🎮","😒","🐕‍🦺","🐂","🕰️","🎙️","🐂","🧐","🎮","🐕‍🦺","🕰️"]
+  baraja =["😒","😍","😍","🎙️","🎮","😒","🐕‍🦺","🐂","🕰️","🎙️","🐂","🧐","🎮","🐕‍🦺","🕰️","🧐"];
 
+    Reinicio() {
+    this.baraja = this.shuffleArray(this.baraja);
+  }
+
+  shuffleArray<T>(array: T[]): T[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  
 }
